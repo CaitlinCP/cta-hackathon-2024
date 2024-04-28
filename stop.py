8696668c4f655f7d2c03b8e26c00940e3349e28d
pravchand <praveenc@uchicago.edu>
2024-04-28 15:50:36 -0500

    stop_walk

---
+++ stop.py
@@ -0,0 +1,44 @@
+import math
+
+EARTH_R_MI = 3963
+
+
+def haversine_distance(location1,location2):
+    """
+    Calculate the distance between two points on a sphere (like Earth) in miles.
+
+    https://en.wikipedia.org/wiki/Haversine_formula
+
+    :param lat1: latitude of first point
+    :param lon1: longitude of first point
+    :param lat2: latitude of second point
+    :param lon2: longitude of second point
+
+    :return: distance in miles
+    """
+    lat1, lon1, lat2, lon2 = location1,location2
+    def deg_to_rad(deg):
+        return deg * math.pi / 180
+    
+    #Using MAP to apply deg_to_rad to each argument
+    lat1, lon1, lat2, lon2 = map(deg_to_rad, [lat1, lon1, lat2, lon2])
+
+    #Haversine_formula
+    distance = 2 * EARTH_R_MI * math.asin(
+        math.sqrt(
+            (math.sin((lat2 - lat1) / 2)**2) +
+            (math.cos(lat1) * math.cos(lat2) * math.sin((lon2 - lon1) / 2)**2)))
+
+    return distance
+
+
+
+class Stop:
+    def __init__(self,id,name,stop_location):
+        stop_id = id
+        stop_name = name 
+        stop_location = stop_location
+        
+    def get_distance(self,dist_location):
+        distance = haversine_distance(self.stop_location,dist_location)
+        return distance
