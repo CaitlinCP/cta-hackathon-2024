9fd5c92ffcd985d16faa61cf7d28c5a001a87479
Caitlin Pratt <cpratt@swingleft.org>
2024-04-28 16:05:23 -0500

    changed to post request

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ The main app for
 
 from flask import Flask, request, render_template, redirect, jsonify
 
+
+def get_nearest_stop(name):
+    pass
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,7 +20,8 @@ def get_route_input():
         bus_info = {
             'bus_line': request.form.get['bus_line'],
             'user_address': request.form.get['user_address'],
-            'use_location': request.form.get['use_location']
+            'use_location': request.form.get['use_location'],
+            'find_nearest_stop': request.form.get['find_nearest_stop']
         }
 
         return redirect("/nearest_stop", bus_info=bus_info)
@@ -28,17 +33,23 @@ def get_route_input():
         "route_input.html", bus_lines=bus_lines
     )
 
-@app.route("/nearest_stop", methods=['GET'])
+@app.route("/nearest_stop", methods=['POST'])
 def get_nearest_stop():
 
     try:
-        bus_line=request.args.get('bus_line')
-        user_address=request.args.get('user_address')
-        use_location=request.args.get('use_location')
+        bus_line=request.form.get('bus_line')
+        user_address=request.form.get('user_address')
+        use_location=request.form.get('use_location')
+        find_nearest_stop=request.form.get('find_nearest_stop')
+    
+    #TODO: Populate list of stops for a line
+        ## Make an api call for the line
+        ## 
+    #TODO: OR, given 'find_nearest_stop' == TRUE -- automatically select nearest stop
+    #
     
     except Exception as error:
-        return jsonify(
-            {
+        return jsonify({
                 'code': 400,
                 'status': 'error',
                 'message': f'an exception occurred: f{error}'
@@ -48,7 +59,8 @@ def get_nearest_stop():
     return jsonify({
         'bus_line': bus_line,
         'user_address': user_address,
-        'use_location': use_location
+        'use_location': use_location,
+        'find_nearest_stop': find_nearest_stop
     })
 
     #TODO: Call functions
